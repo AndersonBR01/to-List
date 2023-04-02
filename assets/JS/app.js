c9ceadaf1d7dd1477e9308e20c7921d679c02197
Anderson <andersoncsilva850@gmail.com>
2023-04-02 19:02:57 -0300

    PARTE 7º
    CRIANDO FUNÇÃO PARA CRIAR UM ITEM DENTRO DO ARRAY E  VARIAVEL DE CAPTURA DE TECLA "ENTER" E PEGAR UM VALOR DIGITADO NA CAIXA DE PESQUISA

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -5,13 +5,10 @@
 //CRIANDO UMA FUNÇÃO DE ATUALIZAÇÃO 5
 //CRIANDO FUNÇÃO DE LIMPAR TELA QUANDO ATUALIZAR A TELA, 6
 // SEMPRE VAI LIMPAR A ULTIMA TAREFA INSERIDA
+//CRIANDO UMA FUNÇÃO PARA CRIAR UM ITEM NO ARRAY 7
 
 //4
-let banco = [
-    {'tarefa': 'Estudar JS', 'status': ''},
-    {'tarefa': 'netflix', 'status' : 'checked'},
-    {'tarefa': 'Jogar Honor Of Kings','status': 'checked'}
-]
+let banco = []
 //1,2,3
 const criarItem = (tarefa,status = '') => {
 
@@ -39,5 +36,15 @@ const atualizarTela = () => {
     banco.forEach (item => criarItem(item.tarefa, item.status));
 }
 
-atualizarTela();
+const inserirItem = (evento) => {
+    const tecla = evento.key;
+    const texto = evento.target.value;
+    if (tecla === 'Enter'){
+        banco.push({'tarefa': texto, 'status': ''})
+        atualizarTela();
+    }
+}
+
+document.getElementById('newItem').addEventListener('keypress',inserirItem);
+
 atualizarTela();
